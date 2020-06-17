6936ca4554759a341299635dec73400b30669ab8
zhBugFly <zhjibupt@gmail.com>
2020-06-17 23:25:22 +0800

    查询各省市数据

--- orangeweather/src/main/java/com/zh/orangeweather/ChooseAreaFragment.java
+++ orangeweather/src/main/java/com/zh/orangeweather/ChooseAreaFragment.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,9 +19,20 @@ import androidx.fragment.app.Fragment;
 import com.zh.orangeweather.db.City;
 import com.zh.orangeweather.db.Country;
 import com.zh.orangeweather.db.Province;
+import com.zh.orangeweather.util.HttpUtil;
+import com.zh.orangeweather.util.Utility;
 
+import org.jetbrains.annotations.NotNull;
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class ChooseAreaFragment extends Fragment {
     public static final int LEVEL_PROVINCE = 0;
@@ -83,27 +95,113 @@ public class ChooseAreaFragment extends Fragment {
     }
 
     private void queryProvinces(){
-
+        titleText.setText("中国");
+        btnBack.setVisibility(View.GONE);
+        provinceList = LitePal.findAll(Province.class);
+        if(provinceList.size() > 0){
+            dataList.clear();
+            for(Province province : provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_PROVINCE;
+        }else{
+            String address = "http://guolin.tech/api/china";
+            queryFromServer(address, "province");
+        }
     }
     private void queryCities(){
-
+        titleText.setText(selectedProvince.getProvinceName());
+        btnBack.setVisibility(View.VISIBLE);
+        cityList = LitePal.where("provinceid = ?", String.valueOf(selectedProvince.getId())).find(City.class);
+        if(cityList.size() > 0){
+            dataList.clear();
+            for(City city: cityList){
+                dataList.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_CITY;
+        }else{
+            int provinceCode = selectedProvince.getProvinceCode();
+            String address = "https://guolin.tech/api/china/" + provinceCode;
+            queryFromServer(address, "city");
+        }
     }
     private void queryCountries(){
-
+        titleText.setText(selectedCity.getCityName());
+        btnBack.setVisibility(View.VISIBLE);
+        countryList = LitePal.where("cityid = ?", String.valueOf(selectedCity.getId())).find(Country.class);
+        if(countryList.size() > 0){
+            dataList.clear();
+            for(Country country : countryList){
+                dataList.add(country.getCountryName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_COUNTRY;
+        }else{
+            int provinceCode = selectedProvince.getProvinceCode();
+            int cityCode = selectedCity.getCityCode();
+            String address = "http://guolin.tech/api/china" + provinceCode + "/" + cityCode;
+            queryFromServer(address, "country");
+        }
     }
 
     private void queryFromServer(String address, final String type){
+        showProgressBar();
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        hideProgressBar();
+                        Toast.makeText(getContext(), "加载失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
 
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+                if("province".equals(type)){
+                    result = Utility.handleProvinceResponse(responseText);
+                }else if("city".equals(type)){
+                    result = Utility.handleCityResponse(responseText, selectedProvince.getId());
+                }else if("country".equals(type)){
+                    result = Utility.handleCountryResponse(responseText, selectedCity.getId());
+                }
+                if(result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            hideProgressBar();
+                            if("province".equals(type)){
+                                queryProvinces();
+                            }else if("city".equals(type)){
+                                queryCities();
+                            }else if("country".equals(type)){
+                                queryCountries();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 
     private void showProgressBar(){
         if(progressBar == null){
             progressBar = new ProgressBar(getActivity());
         }
+        progressBar.setVisibility(View.VISIBLE);
     }
     private void hideProgressBar(){
         if(progressBar != null){
-
+            progressBar.setVisibility(View.GONE);
         }
     }
 
